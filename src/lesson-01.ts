ea6440ab950a65b6e486f59bc5cf83cad50a9091
szabi1112 <banki.szabolcs@gmail.com>
2022-10-19 22:20:31 +0200

    Kész

--- src/lesson-01.ts
+++ src/lesson-01.ts
@@ -1,33 +1,56 @@
 export default class calculator {
-    // A változókat az osztályon belül így hozhatod létre.
-    active: boolean = false;
+  // A változókat az osztályon belül így hozhatod létre.
+  active: boolean = false;
 
-    // Hozd létre a következő változókat a megfelelő típusannotációval.
-    
-    /** @type {Array} points - számok tömbje, 1-től 10-ig egyesével. */
+  // Hozd létre a következő változókat a megfelelő típusannotációval.
 
+  /** @type {Array} points - számok tömbje, 1-től 10-ig egyesével. */
+  points: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-    /** 
-     * A tömb minden eleme legyen egy objektum, amely egy felhasználót reprezentál.
-     * Kötelező elemek: name<string>, email<string>.
-     * Inicializáld a tömböt, legyen legalább két eleme.
-     * @type {Array} users - minden elem egy felhasználó.
-     */
-
-    
-    /** 
-     * A tömb minden eleme legyen egy objektum, amely egy terméket reprezentál.
-     * Kötelező elemek: name<string>, price<number>, stock<number>.
-     * Opcionális elemek: active<boolean>.
-     * Inicializáld a tömböt, legyen legalább két eleme.
-     * @type {Array} products - minden elem egy termék.
-     */    
-
-    
-    /** 
-     * Inicializálj egy tuple típust, azaz adj neki értéket is.
-     * @type {Tuple} role - number, string, boolean. 
-     */
-
-
+  /**
+   * A tömb minden eleme legyen egy objektum, amely egy felhasználót reprezentál.
+   * Kötelező elemek: name<string>, email<string>.
+   * Inicializáld a tömböt, legyen legalább két eleme.
+   * @type {Array} users - minden elem egy felhasználó.
+   */
+  users: Array<{ name: string; email: string }> = [
+    {
+      name: "John Doe",
+      email: "johndore@gmail.com",
+    },
+    {
+      name: "Jane Doe",
+      email: "janeDoe@gmail.com",
+    },
+  ];
+  /**
+   * A tömb minden eleme legyen egy objektum, amely egy terméket reprezentál.
+   * Kötelező elemek: name<string>, price<number>, stock<number>.
+   * Opcionális elemek: active<boolean>.
+   * Inicializáld a tömböt, legyen legalább két eleme.
+   * @type {Array} products - minden elem egy termék.
+   */
+  products: Array<{
+    name: string;
+    price: number;
+    stock: number;
+    active?: boolean;
+  }> = [
+    {
+      name: "vasaló",
+      price: 2000,
+      stock: 4,
+      active: true,
+    },
+    {
+      name: "laptop",
+      price: 100000,
+      stock: 0,
+    },
+  ];
+  /**
+   * Inicializálj egy tuple típust, azaz adj neki értéket is.
+   * @type {Tuple} role - number, string, boolean.
+   */
+  role: [number, string, boolean] = [1, "TV", false];
 }
